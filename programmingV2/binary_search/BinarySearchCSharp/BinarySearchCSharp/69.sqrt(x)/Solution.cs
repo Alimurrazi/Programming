7f19801bd3bf1c232adb97fc8103d5b8a9e9d5eb
Alimur Razi Rana <alimur.razi@selise.ch>
2020-01-26 19:42:48 +0600

    sqrt problem start

--- programmingV2/binary_search/BinarySearchCSharp/BinarySearchCSharp/69.sqrt(x)/Solution.cs
+++ programmingV2/binary_search/BinarySearchCSharp/BinarySearchCSharp/69.sqrt(x)/Solution.cs
@@ -8,8 +8,42 @@ namespace BinarySearchCSharp._69.sqrt_x_
     {
         public int MySqrt(int x)
         {
-            Console.WriteLine(x);
-            return x;
+            int low = 0, high = x;
+            int mid=0, result;
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                result = mid * mid;
+                Console.WriteLine(result);
+                Console.WriteLine("----------");
+                if(result == x)
+                {
+                    break;
+                }
+                else if (result > x)
+                {
+                    high = mid - 1;
+                }
+                else if (result < x)
+                {
+                    low = mid + 1;
+                }
+            }
+            Console.WriteLine($"low=== {low}");
+            Console.WriteLine($"high === {high}");
+            Console.WriteLine($"mid === {mid}");
+            if (low <= high)
+            {
+                return low;
+            }
+            else
+            {
+                return high;
+            }
+          //  return mid;
+
+       //     Console.WriteLine(x);
+        //    return x;
         }
     }
 }
--- programmingV2/binary_search/BinarySearchCSharp/BinarySearchCSharp/Program.cs
+++ programmingV2/binary_search/BinarySearchCSharp/BinarySearchCSharp/Program.cs
@@ -7,14 +7,16 @@ namespace BinarySearchCSharp
     {
         static void Main(string[] args)
         {
-            //   new _69.sqrt_x_.Solution().MySqrt(10);
 
             int result;
-            int[] PositionOfHouses = { 282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923 };
-            int[] PositionOfHeaters = { 823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612 };
-            result = new _475.Heaters.Solution().FindRadius(PositionOfHouses, PositionOfHeaters);
+            result = new _69.sqrt_x_.Solution().MySqrt(4);
 
-          //  Console.WriteLine(result);
+            //int result;
+            //int[] PositionOfHouses = { 282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923 };
+            //int[] PositionOfHeaters = { 823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612 };
+            //result = new _475.Heaters.Solution().FindRadius(PositionOfHouses, PositionOfHeaters);
+
+            Console.WriteLine(result);
         }
     }
 }
