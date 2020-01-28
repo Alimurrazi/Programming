ea1da6c08d614d1cd6dbb5c1960d35c0500ca5ef
Alimur Razi Rana <alimur.razi@selise.ch>
2020-01-28 11:01:13 +0600

    two pointer c# template add

---
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/977.Squares of a Sorted Array/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoPointerCSharp._977.Squares_of_a_Sorted_Array
+{
+    class Solution
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            return A;
+        }
+    }
+}
---
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TwoPointerCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ara = { -4, -1, 0, 3, 10 };
+            new _977.Squares_of_a_Sorted_Array.Solution().SortedSquares(ara);
+        }
+    }
+}
