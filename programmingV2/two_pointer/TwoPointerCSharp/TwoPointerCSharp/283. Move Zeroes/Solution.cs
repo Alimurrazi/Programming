70858ecdbd2137fce02562d3ba0569c19274c657
Alimur Razi Rana <alimur.razi@selise.ch>
2020-01-29 17:12:16 +0600

    move zeroes accepted

---
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/283. Move Zeroes/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoPointerCSharp._283._Move_Zeroes
+{
+    class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int low = 0, high = nums.Length - 1;
+            while (low <= high)
+            {
+                if (nums[low] == 0)
+                {
+                    for(int i = low+1; i <= high; i++)
+                    {
+                        nums[i - 1] = nums[i];
+                    }
+                    nums[high] = 0;
+               //     low++;
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }
+            }
+            foreach(int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
--- programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
@@ -6,8 +6,12 @@ namespace TwoPointerCSharp
     {
         static void Main(string[] args)
         {
-            int[] ara = { -7, -3, 2, 3, 11 };
-            new _977.Squares_of_a_Sorted_Array.Solution().SortedSquares(ara);
+            //int[] ara = { -7, -3, 2, 3, 11 };
+            //new _977.Squares_of_a_Sorted_Array.Solution().SortedSquares(ara);
+
+         //   int[] ara = { 0, 1, 0, 3, 12 };
+            int[] ara = { 1,0,1,0,1,0 };
+            new _283._Move_Zeroes.Solution().MoveZeroes(ara);
         }
     }
 }
