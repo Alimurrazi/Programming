71648c9213d8c2ab258e9b5fea24f7d44aba2b63
Alimur Razi Rana <alimur.razi@selise.ch>
2020-02-06 11:23:51 +0600

    intersection of two array accepted

---
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/350. Intersection of Two Arrays 2/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoPointerCSharp._350._Intersection_of_Two_Arrays_2
+{
+    class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+
+            int maxLength = Math.Min(nums1.Length, nums2.Length);
+            List<int> resultList = new List<int>();
+            int index = 0, startingForNums2 = 0;
+            for(int i = 0; i < nums1.Length; i++)
+            {
+                for(int j = startingForNums2; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        resultList.Add(nums1[i]);
+                        startingForNums2 = j+1;
+                        break;
+                    }
+                }
+            }
+            int[] result = resultList.ToArray();
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            return result;
+        }
+    }
+}
--- programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
@@ -9,9 +9,16 @@ namespace TwoPointerCSharp
             //int[] ara = { -7, -3, 2, 3, 11 };
             //new _977.Squares_of_a_Sorted_Array.Solution().SortedSquares(ara);
 
-         //   int[] ara = { 0, 1, 0, 3, 12 };
-            int[] ara = { 1,0,1,0,1,0 };
-            new _283._Move_Zeroes.Solution().MoveZeroes(ara);
+            //   int[] ara = { 0, 1, 0, 3, 12 };
+            //int[] ara = { 1,0,1,0,1,0 };
+            //new _283._Move_Zeroes.Solution().MoveZeroes(ara);
+
+            int[] nums1 = { 1, 2, 2, 1 };
+            int[] nums2 = { 2, 2 };
+            //int[] nums1 = { 4, 9, 5 };
+            //int[] nums2 = { 9, 4,9,8,4 };
+
+            new _350._Intersection_of_Two_Arrays_2.Solution().Intersect(nums1, nums2);
         }
     }
 }
