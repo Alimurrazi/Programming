3cca6bee774ea38b36ceb9cd6af24284ab532d97
Alimur Razi Rana <alimur.razi@selise.ch>
2020-02-16 15:18:10 +0600

    844 problem accepted

---
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/844. Backspace String Compare/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoPointerCSharp._844._Backspace_String_Compare
+{
+    public class Solution
+    {
+        int Sindex,Tindex,i,j;
+        int GetNonHashIndex(string S, int FirstIndex)
+        {
+            int hashCount = 0;
+            for (i = FirstIndex - 1; i >= 0; i--)
+            {
+                if (S[i] == '#')
+                {
+                    hashCount++;
+                }
+                else
+                {
+                    if (hashCount == 0)
+                    {
+                        return i;
+                    }
+                    else
+                    {
+                        hashCount--;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public bool BackspaceCompare(string S, string T)
+        {
+            Sindex = S.Length;
+            Tindex = T.Length;
+
+            while (Sindex != -1 && Tindex != -1)
+            {
+               Sindex = this.GetNonHashIndex(S, Sindex);
+               Tindex = this.GetNonHashIndex(T, Tindex);
+                if (Sindex == -1 && Tindex == -1)
+                {
+                    return true;
+                }
+                if (Sindex == -1 || Tindex == -1)
+                {
+                    return false;
+                }
+                if (S[Sindex] != T[Tindex])
+                {
+                    return false;
+                }  
+            }
+            return true;
+        }
+    }
+}
--- programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
+++ programmingV2/two_pointer/TwoPointerCSharp/TwoPointerCSharp/Program.cs
@@ -13,12 +13,16 @@ namespace TwoPointerCSharp
             //int[] ara = { 1,0,1,0,1,0 };
             //new _283._Move_Zeroes.Solution().MoveZeroes(ara);
 
-            int[] nums1 = { 1, 2, 2, 1 };
-            int[] nums2 = { 2, 2 };
+            //int[] nums1 = { 1, 2, 2, 1 };
+            //int[] nums2 = { 2, 2 };
             //int[] nums1 = { 4, 9, 5 };
             //int[] nums2 = { 9, 4,9,8,4 };
 
-            new _350._Intersection_of_Two_Arrays_2.Solution().Intersect(nums1, nums2);
+            //  new _350._Intersection_of_Two_Arrays_2.Solution().Intersect(nums1, nums2);
+            string str1 = "a####";
+            string str2 = "a#";
+            bool result = new _844._Backspace_String_Compare.Solution().BackspaceCompare(str1, str2);
+            Console.WriteLine(result);
         }
     }
 }
