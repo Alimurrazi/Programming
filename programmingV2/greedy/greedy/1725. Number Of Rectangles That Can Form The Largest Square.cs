88d5cb920df648214ab794458b386d5a87475490
Alimurrazi <alimurrazi570@gmail.com>
2021-01-20 20:09:28 +0600

    new problems solved

---
+++ programmingV2/greedy/greedy/1725. Number Of Rectangles That Can Form The Largest Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace greedy
+{
+    class _1725
+    {
+        public int CountGoodRectangles(int[][] rectangles)
+        {
+            int maxlen = Math.Min(rectangles[0][0], rectangles[0][1]);
+            int i, maxcount = 0, temp;
+            for (i = 0; i < rectangles.Length; i++)
+            {
+                temp = Math.Min(rectangles[i][0], rectangles[i][1]);
+                if(temp == maxlen)
+                {
+                    maxcount++;
+                } else
+                {
+                    if(temp > maxlen)
+                    {
+                        maxlen = temp;
+                        maxcount = 1;
+                    }
+                }
+            }
+            return maxcount;
+        }
+    }
+}
--- programmingV2/greedy/greedy/Program.cs
+++ programmingV2/greedy/greedy/Program.cs
@@ -6,9 +6,19 @@ namespace greedy
     {
         static void Main(string[] args)
         {
-            int[] ara = { 6 };
-//            Console.WriteLine(new _1217().MinCostToMoveChips(ara));
-            Console.WriteLine(new _1403().MinSubsequence(ara));
+            //           int[] ara = { 6 };
+            // var a = [[5, 8],[3, 9],[5, 12],[16, 5]];
+            int[][] ara = new int[][]
+            {
+               new int[] { 2, 3 },
+               new int[] { 3, 7 },
+               new int[] { 4, 3 },
+               new int[] { 3, 7 }
+            };
+        //    int[,] ara = new int[,] { { 5, 8 }, { 3, 9 }, { 5, 12 }, { 16, 5 } };
+            Console.WriteLine(new _1725().CountGoodRectangles(ara));
+            //            Console.WriteLine(new _1217().MinCostToMoveChips(ara));
+            //  Console.WriteLine(new _1403().MinSubsequence(ara));
         }
     }
 }
