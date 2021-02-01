e968483abb38dd2d59777b62ab525d4ef462e2b5
Alimurrazi <alimurrazi570@gmail.com>
2021-02-01 08:10:43 +0600

    944 problem solved

---
+++ programmingV2/greedy/greedy/944.Delete_Columns_to_Make_Sorted.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace greedy
+{
+    class _944
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int i, count = 0, j = 0;
+            for (i = 0; i < strs[0].Length; i++)
+            {
+                for (j = 0; j < strs.Length-1; j++)
+                {
+                    var a = strs[j][i];
+                    var b = strs[j+1][i];
+                    int firstValue = (int)strs[j][i];
+                    int secondValue = (int)strs[j+1][i];
+                    if (firstValue > secondValue)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
--- programmingV2/greedy/greedy/Program.cs
+++ programmingV2/greedy/greedy/Program.cs
@@ -7,17 +7,18 @@ namespace greedy
         static void Main(string[] args)
         {
             //           int[] ara = { 6 };
-            // var a = [[5, 8],[3, 9],[5, 12],[16, 5]];
-            int[][] ara = new int[][]
-            {
-               new int[] { 2, 3 },
-               new int[] { 3, 7 },
-               new int[] { 4, 3 },
-               new int[] { 3, 7 }
-            };
-        //    int[,] ara = new int[,] { { 5, 8 }, { 3, 9 }, { 5, 12 }, { 16, 5 } };
-            Console.WriteLine(new _1725().CountGoodRectangles(ara));
-            //            Console.WriteLine(new _1217().MinCostToMoveChips(ara));
+            //int[][] ara = new int[][]
+            //{
+            //   new int[] { 2, 3 },
+            //   new int[] { 3, 7 },
+            //   new int[] { 4, 3 },
+            //   new int[] { 3, 7 }
+            //};
+            //    int[,] ara = new int[,] { { 5, 8 }, { 3, 9 }, { 5, 12 }, { 16, 5 } };
+            string[] aras = new string[] { "zyx", "wvu", "tsr" };
+            Console.WriteLine(new _944().MinDeletionSize(aras));
+           // Console.WriteLine(new _1725().CountGoodRectangles(ara));
+            //  Console.WriteLine(new _1217().MinCostToMoveChips(ara));
             //  Console.WriteLine(new _1403().MinSubsequence(ara));
         }
     }
