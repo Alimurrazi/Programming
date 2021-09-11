1aa191298d88c9fbec726ff78da1683f861796bb
Alimur Razi Rana <alimurrazi570@gmail.com>
2021-09-11 10:12:04 +0600

    feat: leet1323 add

---
+++ programmingV2/greedy/greedy/1323. Maximum 69 Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace greedy
+{
+    class _1323
+    {
+        public int Maximum69Number(int num)
+        {
+            int max = num;
+            string converted = num.ToString(), temp;
+            for(int i = 0; i < converted.Length; i++)
+            {
+                temp = converted;
+                if(converted[i] == '6')
+                {
+                    temp = converted.Substring(0, i) + '9' + converted.Substring(i + 1);
+                }
+                else if (converted[i] == '9')
+                {
+                    temp = converted.Substring(0, i) + '6' + converted.Substring(i + 1);
+                }
+                if (Convert.ToInt32(temp) > max)
+                {
+                    max = Convert.ToInt32(temp);
+                }
+            }
+           
+
+            return max;
+        }
+    }
+}
--- programmingV2/greedy/greedy/Program.cs
+++ programmingV2/greedy/greedy/Program.cs
@@ -15,8 +15,12 @@ namespace greedy
             //   new int[] { 3, 7 }
             //};
             //    int[,] ara = new int[,] { { 5, 8 }, { 3, 9 }, { 5, 12 }, { 16, 5 } };
-            string[] aras = new string[] { "zyx", "wvu", "tsr" };
-            Console.WriteLine(new _944().MinDeletionSize(aras));
+            // string[] aras = new string[] { "zyx", "wvu", "tsr" };
+            // Console.WriteLine(new _944().MinDeletionSize(aras));
+            var value = Console.ReadLine();
+            int num = Convert.ToInt32(value);
+            Console.WriteLine(new _1323().Maximum69Number(num));
+
            // Console.WriteLine(new _1725().CountGoodRectangles(ara));
             //  Console.WriteLine(new _1217().MinCostToMoveChips(ara));
             //  Console.WriteLine(new _1403().MinSubsequence(ara));
