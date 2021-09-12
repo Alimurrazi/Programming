7659e38fdb2225f7c00df8c563705d7cc4c3248d
Alimur Razi Rana <alimurrazi570@gmail.com>
2021-09-12 10:01:21 +0600

    feat: leetcode 1827 add

---
+++ programmingV2/greedy/greedy/1827. Minimum Operations to Make the Array Increasing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace greedy
+{
+    class _1827
+    {
+        public int MinOperations(int[] nums)
+        {
+            int res = 0;
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] <= nums[i - 1])
+                {
+                    res = res + (nums[i-1] - nums[i])+1;
+                    nums[i] = nums[i - 1] + 1;
+                }
+            }
+            return res;
+        }
+    }
+}
--- programmingV2/greedy/greedy/Program.cs
+++ programmingV2/greedy/greedy/Program.cs
@@ -17,9 +17,12 @@ namespace greedy
             //    int[,] ara = new int[,] { { 5, 8 }, { 3, 9 }, { 5, 12 }, { 16, 5 } };
             // string[] aras = new string[] { "zyx", "wvu", "tsr" };
             // Console.WriteLine(new _944().MinDeletionSize(aras));
-            var value = Console.ReadLine();
-            int num = Convert.ToInt32(value);
-            Console.WriteLine(new _1323().Maximum69Number(num));
+            //            var value = Console.ReadLine();
+            //          int num = Convert.ToInt32(value);
+            //        Console.WriteLine(new _1323().Maximum69Number(num));
+
+            int[] nums = { 1, 1, 1 };
+            Console.WriteLine(new _1827().MinOperations(nums));
 
            // Console.WriteLine(new _1725().CountGoodRectangles(ara));
             //  Console.WriteLine(new _1217().MinCostToMoveChips(ara));
